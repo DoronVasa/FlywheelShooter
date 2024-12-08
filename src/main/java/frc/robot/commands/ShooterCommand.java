1f3b57d7243f8c1b95dd490d7709c92b6e1b29cd
Doron Vasa <128730878+DoronVasa@users.noreply.github.com>
2024-12-08 23:49:22 +0200

    Add files via upload

---
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterCommand extends Command{
+    private final ShooterSubsystem shooterSubsystem;
+
+    public ShooterCommand(ShooterSubsystem shooterSubsystemConstructor){
+        shooterSubsystem = shooterSubsystemConstructor;
+
+        addRequirements(shooterSubsystemConstructor); // ¯\_(ツ)_/¯ ¯\_(ツ)_/¯ ¯\_(ツ)_/¯ ¯\_(ツ)_/¯
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        shooterSubsystem.MoveMotor(shooterSubsystem.CalculatePID());
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import frc.robot.Constants;
+
+public class ShooterSubsystem {
+    private final TalonFX talonFX = new TalonFX(Constants.SubsystemsConstants.ShooterSubsystemConstants.TALON_SUBSYSTEM_SHOOTER_ID);
+    private final TalonFX invertedTalonFX = new TalonFX(Constants.SubsystemsConstants.ShooterSubsystemConstants.INVERTED_TALON_SUBSYSTEM_SHOTTER_ID);    
+
+    private final PIDController pid = new PIDController(Constants.SubsystemsConstants.ShooterSubsystemConstants.ShooterSubsystemPIDConstants.KP, Constants.SubsystemsConstants.ShooterSubsystemConstants.ShooterSubsystemPIDConstants.KP, Constants.SubsystemsConstants.ShooterSubsystemConstants.ShooterSubsystemPIDConstants.KP); // mhm great names very readable 👍
+
+    public ShooterSubsystem(){
+        this.talonFX.setInverted(false);
+        this.invertedTalonFX.setInverted(true);
+        this.pid.setSetpoint(3000);
+    }
+
+    public double GetSpeedInRPM(){
+        return this.talonFX.getVelocity().getValueAsDouble();
+    }
+
+    public double CalculatePID(){
+        return pid.calculate(GetSpeedInRPM());
+    }
+
+    public void MoveMotor(double speed){
+        this.talonFX.set(speed);
+        this.invertedTalonFX.set(speed);
+    }
+}
